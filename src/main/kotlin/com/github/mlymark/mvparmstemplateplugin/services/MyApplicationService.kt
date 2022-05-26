65bca46de26195d2239790e26c09b62b1682fc80
GitHub Action <action@github.com>
2022-05-26 00:57:59 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mlymark/mvparmstemplateplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mlymark.mvparmstemplateplugin.services
+
+import com.github.mlymark.mvparmstemplateplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
